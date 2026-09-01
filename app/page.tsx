e99a1231ee05335d12d47d0a0c6b46c28ad3b89b
l2sakim <132890581+elle2good@users.noreply.github.com>
2026-09-01 21:00:59 +0900

    Wire Smooth Scroll into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import HomeHeader from "@/components/HomeHeader"
+import SmoothScroll from "@/components/SmoothScroll"
 
 export default function Home() {
   return (
     <main className="home-page">
+      <SmoothScroll intensity={10} />
       <HomeHeader />
+      <div id="overlay" />
     </main>
   )
 }
